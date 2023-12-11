368d8cf3d4f9c2364e973dc23bf4adec1543ad45
Varixx0 <victermug@alu.edu.gva.es>
2023-12-11 09:07:37 +0100

    Archivos clases y enunciados

---
+++ Problema del barbero pitufin/ProblemaDelBarbero.java
@@ -0,0 +1,18 @@
+/*
+ * ENUNCIADO: 
+ * En una barbería, hay un barbero que corta el pelo a los clientes y un conjunto de sillas de
+    espera para los clientes. Si no hay clientes para ser atendidos, el barbero se sienta en su silla
+    y se duerme. Cuando llega un cliente, si el barbero está dormido, el cliente lo despierta y se
+    corta el pelo. Si el barbero está ocupado atendiendo a otros clientes, el cliente nuevo se sienta
+    en una de las sillas de espera. Si todas las sillas de espera están ocupadas, el cliente se va.
+    Una vez que el barbero termina de cortar el pelo a un cliente, el siguiente cliente en la silla de
+    espera se despierta si hay alguno y se corta el pelo, o el barbero se duerme de nuevo si no
+    hay más clientes
+ */
+
+
+public class ProblemaDelBarbero {
+    public static void main(String[] args) {
+        
+    }
+}
